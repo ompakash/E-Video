87f3a0f8759b89da07dfec5cbfa8010f5a09bd3c
ompakash <opytc2@gmail.com>
2021-09-21 23:25:59 +0530

    static file in app

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-from code_om.settings import MEDIA_ROOT, MEDIA_URL 
+from code_om.settings import MEDIA_ROOT, MEDIA_URL, STATIC_URL,STATIC_ROOT
 from courses.views import home,coursePage
+
 
 urlpatterns = [
     path('', home , name = "home"),
     path('course/<str:slug>', coursePage , name = "coursepage"),
-]+ static(MEDIA_URL, document_root=MEDIA_ROOT)
+]
 
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+
+urlpatterns += static(STATIC_URL,document_root=STATIC_ROOT)
