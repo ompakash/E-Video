3dacaa4f08c366e02b830922e231a6990cab95d3
ompakash <opytc2@gmail.com>
2021-09-23 20:20:48 +0530

    video list sorted complete

--- courses/views/courses.py
+++ courses/views/courses.py
@@ -1,8 +1,16 @@
 from django.shortcuts import HttpResponse, render
-from courses.models import Course
+from courses.models import Course, Video
 
 
 def coursePage(request,slug):
     course = Course.objects.get(slug=slug)
-    context = {"course":course}
+    serial_no = request.GET.get('lecture')
+    videos = course.video_set.all ().order_by("serial_no")
+    if serial_no is None:
+        serial_no = 1
+    video = Video.objects.get(serial_no = serial_no, course = course)
+    
+    context = {"course":course,
+        "video":video,
+        "videos":videos}
     return render(request,template_name="courses/course_page.html",context=context)
