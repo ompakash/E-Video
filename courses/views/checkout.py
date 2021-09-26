16af64b1629d0b41dc837ed5dd33b85dd955c023
ompakash <opytc2@gmail.com>
2021-09-26 22:31:16 +0530

    checkout query

--- courses/views/checkout.py
+++ courses/views/checkout.py
@@ -6,5 +6,13 @@ def checkout(request,slug):
     course = Course.objects.get(slug=slug)    
     if not request.user.is_authenticated:
         return redirect("login")
-    context = {"course":course}
+
+    action = request.GET.get('action')
+    order = None
+    if action == 'create_payment':
+        print("Creating Order Object")
+        order = "Order Created"
+
+
+    context = {"course":course,"order":order}
     return render(request,template_name="courses/check_out.html",context=context)
